bc70a130bdbefadbd116fd4bd7a791c39c3b3674
deedeeh <dinahafez.8@gmail.com>
2022-03-12 18:37:17 +0000

    Refactored with parameterized tests

--- src/bubble_tea_roulette_service.test.js
+++ src/bubble_tea_roulette_service.test.js
@@ -1,16 +1,13 @@
 const getRandomBubbleTeaType = require('./bubble_tea_roulette_service');
 
-test('should generate random bubble tea', () => {
-  // Arrange
-  // Stub to calls to Math.random to return 0.2
-  jest.spyOn(global.Math, 'random').mockReturnValue(0.2);
-
-  // Act
-  const bubbleTea = getRandomBubbleTeaType();
-
-  // Assert
-  expect(bubbleTea).toBe('JASMINEMILKTEA');
-
-  // Restore the default Math.random
-  jest.spyOn(global.Math, 'random').mockRestore();
-});
+test.each`
+    input          |         expectedResult 
+    ${0.0}         |       ${'OOLONGMILKTEA'}
+    ${0.2}         |       ${'JASMINEMILKTEA'}
+    ${0.6}         |       ${'PEACHICETEA'}
+    ${0.8}         |       ${'LYCHEEICETEA'}
+  `('gets randomBubbleTeaType $input expects $expectedResult',
+    ({input, expectedResult}) => {
+      jest.spyOn(global.Math, 'random').mockReturnValue(input);
+      expect(getRandomBubbleTeaType()).toBe(expectedResult);
+    });
